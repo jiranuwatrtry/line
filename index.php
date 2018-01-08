996a6c0da6e1df0ddca80af0a025a22621ea3e88
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-08 13:08:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -13,13 +13,14 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			$text2 = $text+"จ้า";
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text2
 				
 			];
 
