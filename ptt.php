733116500e62aa9395486e6084fae8b54cc61d88
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-10 14:47:40 +0700

    Update ptt.php

--- ptt.php
+++ ptt.php
@@ -1,34 +1,66 @@
-<?php
-$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL", // URL ของ webservice
+<html lang="en">
+	<head>
+		<title>PTT WEB SERVICE</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+
+
+
+<style>
+
+*{margin:0;padding:0;}
+
+                        
+
+</style>
+
+  
+</head>                  
+
+<body>
+       <div style="max-width:100%;width:400px;margin:auto;">
+         <h3>ราคานำ้มันวันนี้</h3>
+    
+         <?php 
+         
+         
+         
+         $client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
 		    	array(
 			           "trace"      => 1,		// enable trace to view what is happening
 			           "exceptions" => 0,		// disable exceptions
 			          "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
 		           );
-               
 
-
-         // ตัวแปลที่ webservice ต้องการสำหรับ GetOilPriceResult เป็นวันเดือนปีและ ภาษา  
                $params = array(
-                   'Language' => "th"
+                   'Language' => "en",
+                   'DD' => date('d'),
+                   'MM' => date('m'),
+                   'YYYY' => date('Y')
                );
 
-              // เรียกใช้ method GetOilPrice และ ใส่ตัวแปลเข้าไป 
-              $data = $client->CurrentOilPrice($params);
-              
-              //เก็บตัวแปลผลลัพธ์ที่ได้
-              $ob = $data->CurrentOilPriceResult;
-              
-             // เนื่องจากข้อมูลที่ได้เป็น string(ในรูปแบบ xml) จึงต้องแปลงเป็น object ให้ง่ายต่อการเข้าถึง
-              $xml = new SimpleXMLElement($ob);
+		        $data = $client->GetOilPrice($params);
+              $ob = $data->GetOilPriceResult;
+            $xml = new SimpleXMLElement($ob);
            
-             // attr  PRICE_DATE , PRODUCT ,PRICE
-            //loop เพื่อแสดงผล  
-            foreach ($xml  as  $key =>$val) {  
-            
-              // ถ้าไม่มีราคาก็ไม่ต้องแสดงผล เนื่องจากมีบางรายการไม่มีราคา   
-              if($val->PRICE != ''){
+               // PRICE_DATE , PRODUCT ,PRICE
+              foreach ($xml  as  $key =>$val) {  
+              
+            if($val->PRICE != ''){
               echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
                 }
 
                }
+         
+         
+         ?>
+           
+   </div>
+  
+</body>
+
+
+    
+    
+</html>
+    
