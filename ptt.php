afbd9d3c84d094a2367e74c0792c507d2ca58b5b
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-10 15:39:11 +0700

    Update ptt.php

--- ptt.php
+++ ptt.php
@@ -1,46 +1,67 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>PTT WEB SERVICE</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
-// สร้าง object 
-// URL ของ webservice
-$client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL", 
-	array(
-		   "trace"      => 1,	// enable trace to view what is happening
-		   "exceptions" => 0,	// disable exceptions
-		  "cache_wsdl" => 0) 	// disable any caching on the wsdl, encase you alter the wsdl server
-	   );
-               
-$lang = 'en';
-$day = '11';
-$month ='01';
-$year ='2018';
 
-// ตัวแปลที่ webservice ต้องการสำหรับ GetOilPriceResult เป็นวันเดือนปีและ ภาษา  
-$params = array(
-	'Language' => "en",
-	'DD' => date($day),
-    'MM' => date($month),
-    'YYYY' => date($year)
-    );
 
-// เรียกใช้ method GetOilPrice และ ใส่ตัวแปลเข้าไป 
-$data = $client->GetOilPrice($params);
+<style>
 
-//เก็บตัวแปลผลลัพธ์ที่ได้
-$ob = $data->GetOilPriceResult;
+*{margin:0;padding:0;}
 
-// เนื่องจากข้อมูลที่ได้เป็น string(ในรูปแบบ xml) จึงต้องแปลงเป็น object ให้ง่ายต่อการเข้าถึง
-$xml = new SimpleXMLElement($ob);
+                        
 
-// attr  PRICE_DATE , PRODUCT ,PRICE
+</style>
 
-echo "Language = $lang , Date = $day $month $year <br><br>";
-//loop เพื่อแสดงผล
-foreach ($xml  as  $key =>$val) {
-	// ถ้าไม่มีราคาก็ไม่ต้องแสดงผล เนื่องจากมีบางรายการไม่มีราคา   
-	if($val->PRICE != ''){
-	echo $val->PRODUCT .'  '.$val->PRICE.' baht.<br>';
-	}
+  
+</head>                  
 
-}
+<body>
+       <div style="max-width:100%;width:400px;margin:auto;">
+         <h3>ราคานำ้มันวันนี้</h3>
+    
+         <?php 
+         
+         
+         
+         $client = new SoapClient("http://www.pttplc.com/webservice/pttinfo.asmx?WSDL",
+		    	array(
+			           "trace"      => 1,		// enable trace to view what is happening
+			           "exceptions" => 0,		// disable exceptions
+			          "cache_wsdl" => 0) 		// disable any caching on the wsdl, encase you alter the wsdl server
+		           );
 
-?>
+               $params = array(
+                   'Language' => "en",
+                   'DD' => date('d'),
+                   'MM' => date('m'),
+                   'YYYY' => date('Y')
+               );
+
+		        $data = $client->GetOilPrice($params);
+              $ob = $data->GetOilPriceResult;
+            $xml = new SimpleXMLElement($ob);
+           
+               // PRICE_DATE , PRODUCT ,PRICE
+              foreach ($xml  as  $key =>$val) {  
+              
+            if($val->PRICE != ''){
+              echo $val->PRODUCT .'  '.$val->PRICE.' บาท<br>';
+                }
+
+               }
+         
+         
+         ?>
+           
+   </div>
+  
+</body>
+
+
+    
+    
+</html>
+    
