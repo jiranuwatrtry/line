85b0fb504a4b58d57ccea8f07fa34c3a70d348f8
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-11 09:10:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text." : ก็สนธยาไง"
+				'text' => $text." : รับทราบครับ"
 				
 			];
 
