a76f2110e870fcf411adf0dfaa8fb529a59fa8a4
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-15 13:54:55 +0700

    Update ptts.php

--- ptts.php
+++ ptts.php
@@ -1,6 +1,11 @@
-
+<html>
+  <head>
+  </head>
+  <boby>
 <?php 
 // Change to the URL you want to redirect to 
 <iframe frameborder=0 width="295" height="460" scrolling="no" src=http://www.pttplc.com/th/getoilprice.aspx></iframe>
 
 ?>
+</boby>
+</html>
