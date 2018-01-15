710e94a04d918fb6c4e7b4ae66d1e6ed14a18a5b
jiranuwatrtry <jiranuwat.song56@sskru.ac.th>
2018-01-15 14:00:33 +0700

    Update ptts.php

--- ptts.php
+++ ptts.php
@@ -1,11 +1,10 @@
 <html>
-  <head>
-  </head>
   <body>
 <?php 
-// Change to the URL you want to redirect to 
-<iframe frameborder=0 width="295" height="460" scrolling="no" src=http://www.pttplc.com/th/getoilprice.aspx></iframe>
+<div style="max-width:100%;width:400px;margin:auto;">
+<iframe frameborder=0 width="295" height="460" scrolling="no" src=http://www.pttplc.com/th/getoilprice.aspx> </iframe>
 
 ?>
+ </div>   
 </body>
 </html>
